2405050a1c66d54fdf8cf4b6408814b355cc5e7c
Dmitry Dzhus <dima@dzhus.org>
2011-11-29 14:01:50 +0300

    Intersection hit code, indentation changes, Time alias for Double

--- DSMC.hs
+++ DSMC.hs
@@ -6,6 +6,8 @@ type Point = (Double, Double, Double)
 
 type Vector = (Double, Double, Double)
 
+type Time = Double
+
 data Particle = Particle
                 {
                   position :: Point,
@@ -54,23 +56,51 @@ move t (Particle p speed) = (Particle (p <+> (speed *> t)) speed)
 
 inDomain :: Domain -> Particle -> Bool
 inDomain (Box xmin xmax ymin ymax zmin zmax) (Particle (x, y, z) v) =
-         xmax >= x && x >= xmin &&
-         ymax >= y && y >= ymin &&
-         zmax >= z && z >= zmin         
+    xmax >= x && x >= xmin &&
+    ymax >= y && y >= ymin &&
+    zmax >= z && z >= zmin         
 
 clip domain particles = filter (inDomain domain) particles
 
--- Calculate time until object is hit by particle. If negative, then
+-- Normalized normal vector to surface at point
+normal :: Object -> Point -> Vector
+normal (Plane (a, b, c, d)) _ = normalize (a, b, c)
+
+-- Calculate time until object is hit by particle. If not positive, then
 -- particle is inside the object.
-timeToHit :: Particle -> Object -> Double
+timeToHit :: Particle -> Object -> Time
 timeToHit (Particle (x, y, z) (vx, vy, vz)) (Plane (a, b, c, d)) =
-          - (a * x + b * y + c * z + d) / (a * vx + b * vy + c * vz)
+    - (a * x + b * y + c * z + d) / (a * vx + b * vy + c * vz)
 
 -- Update particle position and velocity after specular hit given a
 -- normalized normal vector of surface in hit point and time since hit
--- (negative)
-reflectSpecular :: Particle -> Vector -> Double -> Particle
+-- (not positive)
+reflectSpecular :: Particle -> Vector -> Time -> Particle
 reflectSpecular p n t =
-                move (-1 * t) (move t p){speed = v <-> (n *> (v <*> n) *> 2)}
-                where
-                  v = speed p
+    move (-1 * t) (move t p){speed = v <-> (n *> (v <*> n) *> 2)}
+    where
+      v = speed p
+
+-- Return Maybe 2-tuple containing time until with hit body and closest
+-- object to hit. If no objects are hit so far, return Nothing.
+hit :: Particle -> Body -> Maybe (Time, Object)
+hit p (Primitive o) = 
+    let
+        t = timeToHit p o 
+    in
+      if t > 0
+      then Nothing
+      else Just (t, o)
+
+hit p (Intersection b1 b2) =
+    let
+        r1 = hit p b1
+        r2 = hit p b2
+    in
+      case (r1, r2) of
+        (Just (t1, o1), Just (t2, o2)) -> if abs t1 < abs t2
+                                          then r1
+                                          else r2
+        _ -> Nothing
+                                              
+           
