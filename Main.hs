a218777aa35d544a6c7e7a23270119c780ec2e8a
Dmitry Dzhus <dima@dzhus.org>
2011-12-13 21:18:05 +0300

    Runner redux, processParticles function outlined

--- Main.hs
+++ Main.hs
@@ -10,13 +10,31 @@ exportParticles :: [Particle] -> IO ()
 exportParticles particles = do
      mapM_ (putStrLn . plotParticle) particles
 
+processParticles :: [Particle] -> Time -> Body -> [Particle]
+processParticles particles dt body =
+    clipBody body (map (\p -> hit (move dt p) dt body) particles)
+
+fillBody b = clipBody b [Particle (x / 10, y / 10, z / 10) (0, 0, 0) 
+                             | x <- [-50..50], y <- [-50..50], z <- [-50..50]]
+
 main =
      let
-        plane1 = Primitive (Plane (0, -1, 1) (3))
-        plane2 = Primitive (Plane (0, 0, 1) (-7))
-        plane3 = Primitive (Plane (0, 1, 1) (3))
-        sphere = Primitive (Sphere (0, 1, 0) 2)
-        body = Union (Union plane1 plane3) (sphere)
-        dt = 0.1
+        plane1 = Primitive (Plane (1.5, 0, 1) 0)
+        plane2 = Primitive (Plane (0, -1, 1) 0)
+        plane3 = Primitive (Plane (-0.8, 0.0, 1) 10)
+        sphere1 = Primitive (Sphere (2, 0, 0) 3)
+        sphere2 = Primitive (Sphere (2.1, -1, -0) 3)
+        body = (Union plane1 sphere1)
+        dt = 0.05
+        tmax = 5
+        particles = [Particle (x / 2, y / 2, 10) (0, 0, -5) | x <- [-20..20], y <- [-20..20]]
      in do
-     mapM_ (\p1 -> exportParticles (unfoldr (\(t, p) -> if t > 0 then Just (hit (move dt p) body, (t - dt, hit (move dt p) body)) else Nothing) (3, p1))) [Particle (x / 5, y / 5, 5) (0, 0, -5) | y <- [-10..10], x <- [-10..10]]
+--       exportParticles (fillBody body)
+
+       mapM_ exportParticles (unfoldr (\(t, pcs) -> if t > 0 
+                                                    then 
+                                                        let
+                                                            pNew = processParticles pcs dt body
+                                                        in
+                                                          Just (pNew, (t - dt, pNew)) 
+                                                    else Nothing) (tmax, particles))
