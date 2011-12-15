e34780d3214f823ccee58d460526ae34b1ca2ec4
Dmitry Dzhus <dima@dzhus.org>
2011-12-15 19:20:58 +0300

    Vector operations for Render

--- Vector.hs
+++ Vector.hs
@@ -12,6 +12,8 @@ type Vector = (Double, Double, Double)
 (x, y, z) *> f = (x * f, y * f, z * f)
 (x1, y1, z1) <*> (x2, y2, z2) = x1 * x2 + y1 * y2 + z1 * z2
 
+(x1, y1, z1) <×> (x2, y2, z2) = (y1 * z2 - y2 * z1, x2 * z1 - x1 * z2, x1 * y2 - x2 * y1)
+
 distance :: Point -> Point -> Double
 distance (x1, y1, z1) (x2, y2, z2) = sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2)
 
@@ -22,3 +24,16 @@ normalize :: Vector -> Vector
 normalize v = v *> (1 / norm v)
 
 reverse v = v *> (-1)
+
+-- Build an ortogonal vector parallel to xy plane (rotated CW around
+-- Oz)
+horizontalShifter :: Vector -> Vector
+horizontalShifter (nx, ny, nz) = if nx /= 0
+                                 then (-ny * y / nx, y, 0)
+                                 else (-ny, 0, 0)
+                                     where y = if nx >= 0 then -1 else 1
+
+buildCartesian :: Vector -> (Vector, Vector, Vector)
+buildCartesian v = (normalize v, normalize x, normalize y)
+    where x = horizontalShifter v
+          y = x <×> v
