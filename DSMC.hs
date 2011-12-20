6a92c8fb2175e26175f47754788a5e6d6ce3f92a
Dmitry Dzhus <dima@dzhus.org>
2011-12-20 18:15:09 +0300

    Macroscopic speed calculation routines

--- DSMC.hs
+++ DSMC.hs
@@ -217,3 +217,31 @@ hit dt b p =
             reflectSpecular particleAtHit (fromJust n) t
 
 clipBody body particles = filter (\p -> not (insideBody p body)) particles
+
+-- Macroscopic parameters
+
+data Cell = Cell Point [Particle]
+
+data Speed = Speed Point Vector
+
+circularCells :: Domain -> [Particle] -> Double -> [Cell]
+circularCells (Box xmin xmax ymin ymax zmin zmax) particles radius =
+    [let
+        cx = xmin + x * radius
+        cy = ymin + y * radius
+        cz = zmin + z * radius
+        c = (cx, cy, cz)
+     in
+       (Cell c (filter (\p -> (distance (position p) c) < radius) particles))
+           | x <- [0 .. (xmax - xmin) / radius],
+             y <- [0 .. (ymax - ymin) / radius],
+             z <- [0 .. (zmax - zmin) / radius]]
+
+sampleSpeed :: Cell -> Speed
+sampleSpeed (Cell point particles) =
+    Speed point (averageSpeed particles)
+
+-- Calculate average speed in a cell
+averageSpeed :: [Particle] -> Vector
+averageSpeed particles =
+    (foldl (<+>) (0, 0, 0) (map speed particles)) *> (1 / fromIntegral (length particles))
