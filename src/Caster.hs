d7208b9e2888f38299d40a6811c813d6ce0958dc
Dmitry Dzhus <dima@dzhus.org>
2012-03-11 18:45:37 +0400

    Stub yet

--- src/Caster.hs
+++ src/Caster.hs
@@ -1,20 +1,27 @@
+-- | Standalone raycasting runner for traceable objects.
+
+module Caster
+
+where
+
 import DSMC
 import Render
 
 main = 
-    let
-        plane1 = Primitive (Plane (0, 0, 1) 0)
-        plane2 = Primitive (Plane (0, 0, -1) (-1))
-        plane3 = Primitive (Plane (0, 1, 1) 0)
-        plane4 = Primitive (Plane (1, -1, 1) 0)
-        sphere1 = Primitive (Sphere (0, 0, 0) 4)
-        sphere2 = Primitive (Sphere (0, 0, 1) 4)
-        sphere3 = Primitive (Sphere (1, 0, 1) 4)
-        body = Intersection [sphere1, sphere2, sphere3]
-        camera = Camera (0, 1, 0) (-15)
-        x = 300
-        y = 300
-        scale = 0.03
-    in
-      do
-        putStrLn (renderBodyPgm camera body x y scale)
+    -- let
+    --     plane1 = Primitive (Plane (0, 0, 1) 0)
+    --     plane2 = Primitive (Plane (0, 0, -1) (-1))
+    --     plane3 = Primitive (Plane (0, 1, 1) 0)
+    --     plane4 = Primitive (Plane (1, -1, 1) 0)
+    --     sphere1 = Primitive (Sphere (0, 0, 0) 4)
+    --     sphere2 = Primitive (Sphere (0, 0, 1) 4)
+    --     sphere3 = Primitive (Sphere (1, 0, 1) 4)
+    --     body = Intersection [sphere1, sphere2, sphere3]
+    --     camera = Camera (0, 1, 0) (-15)
+    --     x = 300
+    --     y = 300
+    --     scale = 0.03
+    -- in
+    --   do
+    --     putStrLn (renderBodyPgm camera body x y scale)
+    print "Not implemented yet"
