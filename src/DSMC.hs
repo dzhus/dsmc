25dca1615cf7ec46fb996ab0921568ac287ca7a5
Dmitry Dzhus <dima@dzhus.org>
2012-03-24 00:47:44 +0400

    Tidy up DSMC module

--- src/DSMC.hs
+++ src/DSMC.hs
@@ -1,8 +1,14 @@
+{-|
+  
+  Simulation procedures.
+
+-}
 module DSMC
+    ( processParticles
+    )
 
 where
 
-import Control.Monad
 import Data.Maybe
 
 import DSMC.Particles
@@ -11,7 +17,7 @@ import DSMC.Util
 import DSMC.Util.Vector
 
 
--- Update particle position and velocity after specular hit given a
+-- | Update particle position and velocity after specular hit given a
 -- normalized normal vector of surface in hit point and time since hit
 -- (not positive)
 reflectSpecular :: Particle -> Vector -> Time -> Particle
@@ -20,11 +26,14 @@ reflectSpecular p n t =
     where
       v = velocity p
 
--- A very small amount of time for which the particle is moved after
+
+-- | A very small amount of time for which the particle is moved after
 -- reflecting from body surface.
+hitShift :: Time
 hitShift = 10e-10
 
--- | Particle after possible collision with body during timestep
+
+-- | Particle after possible collision with body during timestep.
 hit :: Time -> Body -> Particle -> Particle
 hit dt b p = 
     let
@@ -43,9 +52,16 @@ hit dt b p =
           in
             move hitShift (reflectSpecular particleAtHit (fromJust n) t)
 
+
+-- | Remove particles which accidentally ended up inside body.
+clipBody :: Body -> [Particle] -> [Particle]
 clipBody body particles = filter (\p -> not (inside body p)) particles
 
--- | Collisionless flow simulation step
+-- | Collisionless flow simulation step.
+--
+-- Given a list of particles, time step Δt and body, linearly move all
+-- particles for Δt, then possibly calculate new particle velocities
+-- and positions wrt body structure.
 processParticles :: [Particle] -> Time -> Body -> [Particle]
 processParticles particles dt body =
     clipBody body (map ((hit dt body) . (move dt)) particles)
