662daf71db7d6de2491d0b5a473275b256a916da
Dmitry Dzhus <dima@dzhus.org>
2012-03-25 22:42:06 +0400

    No shadows

--- src/DSMC/Particles.hs
+++ src/DSMC/Particles.hs
@@ -14,5 +14,5 @@ import DSMC.Util.Vector
 
 -- | Linearly move particle for t time and update its position.
 move :: Time -> Particle -> Particle
-move dt p@(Particle pos velocity) =
-    p{position = pos + (velocity `scale` dt)}
+move dt p@(Particle pos v) =
+    p{position = pos + (v `scale` dt)}
