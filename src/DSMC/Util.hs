829ffe1019127cb6f07393fa16fb2bee7da91515
Dmitry Dzhus <dima@dzhus.org>
2012-05-08 02:00:59 +0400

    Order quadratic equation roots

--- src/DSMC/Util.hs
+++ src/DSMC/Util.hs
@@ -13,12 +13,14 @@ solveq :: (Double, Double, Double)
        -- ^ Coefficients
        -> Maybe (Double, Double)
 solveq (a, b, c)
-    | (d >  0) = Just (r - s, r + s)
+    | (d >  0) = Just (min r1 r2, max r1 r2)
     | (d <= 0) = Nothing
     where
-      d =   b * b - 4 * a * c
-      q =   sqrt d
-      t =   2 * a
-      r = - b / t
-      s =   q / t
+      d  =   b * b - 4 * a * c
+      q  =   sqrt d
+      t  =   2 * a
+      r  = - b / t
+      s  =   q / t
+      r1 =   r - s
+      r2 =   r + s
 {-# INLINE solveq #-}
