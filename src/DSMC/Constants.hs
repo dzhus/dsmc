50066e4483f5f3600ef2a62c2f257eb1512a9350
Dmitry Dzhus <dima@dzhus.org>
2012-06-04 03:26:49 +0400

    Avogadro constant

--- src/DSMC/Constants.hs
+++ src/DSMC/Constants.hs
@@ -1,9 +1,16 @@
 -- | Physical constants.
 
 module DSMC.Constants
-    (boltzmann)
+    (avogadro,
+     boltzmann)
 
 where
+
+
+-- | Avogadro constant 6.02214129(27)e23
+avogadro :: Double
+avogadro = 6.0221412927272727e23
+
 
 -- | Boltzmann constant 1.3806488(13)e-23
 boltzmann :: Double
