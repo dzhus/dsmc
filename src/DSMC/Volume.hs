8a37c71458b884fd264f7b105363b41e7d3dd65e
Dmitry Dzhus <dima@dzhus.org>
2012-06-03 14:18:22 +0400

    Domain → Volume

---
+++ src/DSMC/Volume.hs
@@ -0,0 +1,27 @@
+{-|
+
+Volume operations
+
+-}
+
+module DSMC.Volume
+    ( clipVolume
+    )
+
+where
+
+import DSMC.Types
+import DSMC.Util.Vector
+
+
+-- | Test if particle is in domain.
+inVolume :: Volume -> Particle -> Bool
+inVolume (Box xmin xmax ymin ymax zmin zmax) (Particle (Vec3 x y z) _) =
+    xmax >= x && x >= xmin &&
+    ymax >= y && y >= ymin &&
+    zmax >= z && z >= zmin
+
+
+-- | Filter out particles which are not in domain.
+clipVolume :: Volume -> [Particle] -> [Particle]
+clipVolume vol particles = filter (inVolume vo) particles
