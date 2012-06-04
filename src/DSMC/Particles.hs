b5eabc82ea1c52a7a940992a2e835180e0d033cc
Dmitry Dzhus <dima@dzhus.org>
2012-06-04 19:49:35 +0400

    Added forgotten ParEnsemble decl

--- src/DSMC/Particles.hs
+++ src/DSMC/Particles.hs
@@ -10,12 +10,14 @@ Particle operations.
 module DSMC.Particles
     ( Particle
     , Ensemble
+    , ParEnsemble
     , move
     , printEnsemble
     )
 
 where
 
+import qualified Data.Array.Repa as R
 import qualified Data.Vector.Unboxed as VU
 
 import DSMC.Types
@@ -29,10 +31,15 @@ type Particle = (Point, Vec3)
 -- | Linearly move particle for t time and update its position.
 move :: Time -> Particle -> Particle
 move dt (pos, v) = (pos <+> (v .^ dt), v)
+{-# INLINE move #-}
 
 
 -- | Array of particles.
 type Ensemble = VU.Vector Particle
+
+
+-- | Repa array of particles.
+type ParEnsemble = R.Array R.U R.DIM1 Particle
 
 
 -- | Print particles, one per row, using the format:
