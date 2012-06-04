812f7f0b3bef760f0047d7f5d7951394f1d31530
Dmitry Dzhus <dima@dzhus.org>
2012-06-04 17:20:43 +0400

    +openBoundaryInjection

--- src/DSMC/Domain.hs
+++ src/DSMC/Domain.hs
@@ -8,7 +8,8 @@ Domain operations
 
 module DSMC.Domain
     ( Domain(..)
-    , makeBox
+    , makeDomain
+    , openBoundaryInjection
     , spawnParticles
     )
 
@@ -29,7 +30,7 @@ import DSMC.Util.Vector
 
 -- | Domain in which particles are spawned or system evolution is
 -- simulated.
-data Domain = Box !Double !Double !Double !Double !Double !Double
+data Domain = Domain !Double !Double !Double !Double !Double !Double
             -- ^ Rectangular volume, given by min/max value on every
             -- dimension.
               deriving Show
@@ -37,7 +38,7 @@ data Domain = Box !Double !Double !Double !Double !Double !Double
 
 -- | Test if particle is in domain.
 inDomain :: Domain -> Particle -> Bool
-inDomain !(Box xmin xmax ymin ymax zmin zmax) !((x, y, z), _) =
+inDomain !(Domain xmin xmax ymin ymax zmin zmax) !((x, y, z), _) =
     xmax >= x && x >= xmin &&
     ymax >= y && y >= ymin &&
     zmax >= z && z >= zmin
@@ -45,16 +46,16 @@ inDomain !(Box xmin xmax ymin ymax zmin zmax) !((x, y, z), _) =
 
 -- | Create a rectangular domain with center in the given point and
 -- dimensions.
-makeBox :: Point 
+makeDomain :: Point
         -- ^ Center point.
         -> Double
         -- ^ X dimension.
         -> Double
         -- ^ Y dimension.
-        -> Double 
+        -> Double
         -- ^ Z dimension.
         -> Domain
-makeBox !(x, y, z) !w !l !h =
+makeDomain !(x, y, z) !w !l !h =
     let
         xmin = x - w / 2
         ymin = y - l / 2
@@ -63,16 +64,29 @@ makeBox !(x, y, z) !w !l !h =
         ymax = y + l / 2
         zmax = z + h / 2
     in
-      Box xmin xmax ymin ymax zmin zmax
+      Domain xmin xmax ymin ymax zmin zmax
+
+
+-- | Calculate width, length and height of a domain, which are
+-- dimensions measured by x, y and z axes, respectively.
+getDimensions :: Domain -> (Double, Double, Double)
+getDimensions !(Domain xmin xmax ymin ymax zmin zmax) =
+    (xmax - xmin, ymax - ymin, zmax - zmin)
+
+
+-- | Calculate geometric center of a domain.
+getCenter :: Domain -> Point
+getCenter !(Domain xmin xmax ymin ymax zmin zmax) =
+    (xmin + (xmax - xmin) / 2, ymin + (ymax - ymin) / 2, zmin + (zmax - zmin) / 2)
 
 
 -- | Measure volume of domain.
 volume :: Domain -> Double
-volume !(Box xmin xmax ymin ymax zmin zmax) =
+volume !(Domain xmin xmax ymin ymax zmin zmax) =
     (xmax - xmin) * (ymax - ymin) * (zmax - zmin)
 
 
--- | Sample new particles in domain.
+-- | Sample new particles inside a domain.
 spawnParticles :: PrimMonad m =>
                   Gen (PrimState m)
                -> Domain
@@ -81,11 +95,11 @@ spawnParticles :: PrimMonad m =>
                -> Double
                -- ^ Thermodynamic temperature.
                -> Double
-               -- ^ Molecular mass.
+               -- ^ Mass of molecule.
                -> Vec3
                -- ^ Flow velocity.
                -> m Ensemble
-spawnParticles !g !d@(Box xmin xmax ymin ymax zmin zmax) !n !t !m !(u0, v0, w0) =
+spawnParticles !g !d@(Domain xmin xmax ymin ymax zmin zmax) !n !t !m !(u0, v0, w0) =
     let
         !s = sqrt $ boltzmann * t / m
         !count = round $ n * (volume d)
@@ -97,4 +111,60 @@ spawnParticles !g !d@(Box xmin xmax ymin ymax zmin zmax) !n !t !m !(u0, v0, w0) 
          x <- uniformR (xmin, xmax) g
          y <- uniformR (ymin, ymax) g
          z <- uniformR (zmin, zmax) g
-         return ((x, y, z), (u, v, w))
+         return $! ((x, y, z), (u, v, w))
+
+
+-- | Sample new particles in 6 interface domains along each side of
+-- rectangular simulation domain.
+--
+--
+-- This function implements open boundary condition for
+-- three-dimensional simulation domain.
+--
+-- Interface domains are built on faces of simulation domain using
+-- extrusion along the outward normal of the face.
+--
+-- In 2D projection:
+-- >          +-----------------+
+-- >          |    Interface1   |
+-- >       +--+-----------------+--+
+-- >       |I3|    Simulation   |I4|
+-- >       |  |      domain     |  |
+-- >       +--+-----------------+--+
+-- >          |        I2       |
+-- >          +-----------------+
+openBoundaryInjection :: PrimMonad m =>
+                         Gen (PrimState m)
+                      -> Domain
+                      -- ^ Simulation domain.
+                      -> Double
+                      -- ^ Interface domain extrusion length.
+                      -> Double
+                      -- ^ Concentration.
+                      -> Double
+                      -- ^ Thermodynamic temperature.
+                      -> Double
+                      -- ^ Mass of a molecule.
+                      -> Vec3
+                      -- ^ Flow velocity.
+                      -> m Ensemble
+openBoundaryInjection !g !d@(Domain xmin xmax ymin ymax zmin zmax)
+                      !ex !n !t !m !flow =
+    let
+        (w, l, h) = getDimensions d
+        (cx, cy, cz) = getCenter d
+        d1 = makeDomain (cx - (w + ex) / 2, cy, cz) ex l h
+        d2 = makeDomain (cx + (w + ex) / 2, cy, cz) ex l h
+        d3 = makeDomain (cx, cy + (l + ex) / 2, cz) w ex h
+        d4 = makeDomain (cx, cy - (l + ex) / 2, cz) w ex h
+        d5 = makeDomain (cx, cy, cz - (h + ex) / 2) w l ex
+        d6 = makeDomain (cx, cy, cz + (h + ex) / 2) w l ex
+        (++) = (VU.++)
+    in do
+      v1 <- spawnParticles g d1 n t m flow
+      v2 <- spawnParticles g d2 n t m flow
+      v3 <- spawnParticles g d3 n t m flow
+      v4 <- spawnParticles g d4 n t m flow
+      v5 <- spawnParticles g d5 n t m flow
+      v6 <- spawnParticles g d6 n t m flow
+      return $ v1 ++ v2 ++ v3 ++ v4 ++ v5 ++ v6
