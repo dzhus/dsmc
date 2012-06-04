26add80f498ef00c7fff0ed7daf2452f5c9c54ca
Dmitry Dzhus <dima@dzhus.org>
2012-06-04 19:28:24 +0400

    +clipToDomain

--- src/DSMC/Domain.hs
+++ src/DSMC/Domain.hs
@@ -9,6 +9,7 @@ Domain operations
 module DSMC.Domain
     ( Domain(..)
     , makeDomain
+    , clipToDomain
     , openBoundaryInjection
     , spawnParticles
     )
@@ -17,6 +18,7 @@ where
 
 import Control.Monad.Primitive (PrimMonad, PrimState)
 
+import qualified Data.Array.Repa as R
 import qualified Data.Vector.Unboxed as VU
 
 import System.Random.MWC
@@ -34,14 +36,6 @@ data Domain = Domain !Double !Double !Double !Double !Double !Double
             -- ^ Rectangular volume, given by min/max value on every
             -- dimension.
               deriving Show
-
-
--- | Test if particle is in domain.
-inDomain :: Domain -> Particle -> Bool
-inDomain !(Domain xmin xmax ymin ymax zmin zmax) !((x, y, z), _) =
-    xmax >= x && x >= xmin &&
-    ymax >= y && y >= ymin &&
-    zmax >= z && z >= zmin
 
 
 -- | Create a rectangular domain with center in the given point and
@@ -65,6 +59,7 @@ makeDomain !(x, y, z) !w !l !h =
         zmax = z + h / 2
     in
       Domain xmin xmax ymin ymax zmin zmax
+{-# INLINE makeDomain #-}
 
 
 -- | Calculate width, length and height of a domain, which are
@@ -72,21 +67,26 @@ makeDomain !(x, y, z) !w !l !h =
 getDimensions :: Domain -> (Double, Double, Double)
 getDimensions !(Domain xmin xmax ymin ymax zmin zmax) =
     (xmax - xmin, ymax - ymin, zmax - zmin)
+{-# INLINE getDimensions #-}
 
 
 -- | Calculate geometric center of a domain.
 getCenter :: Domain -> Point
 getCenter !(Domain xmin xmax ymin ymax zmin zmax) =
     (xmin + (xmax - xmin) / 2, ymin + (ymax - ymin) / 2, zmin + (zmax - zmin) / 2)
+{-# INLINE getCenter #-}
 
 
 -- | Measure volume of domain.
 volume :: Domain -> Double
 volume !(Domain xmin xmax ymin ymax zmin zmax) =
     (xmax - xmin) * (ymax - ymin) * (zmax - zmin)
+{-# INLINE volume #-}
 
 
 -- | Sample new particles inside a domain.
+--
+-- PRNG state implies this to be a monadic action.
 spawnParticles :: PrimMonad m =>
                   Gen (PrimState m)
                -> Domain
@@ -117,7 +117,6 @@ spawnParticles !g !d@(Domain xmin xmax ymin ymax zmin zmax) !n !t !m !(u0, v0, w
 -- | Sample new particles in 6 interface domains along each side of
 -- rectangular simulation domain.
 --
---
 -- This function implements open boundary condition for
 -- three-dimensional simulation domain.
 --
@@ -133,6 +132,8 @@ spawnParticles !g !d@(Domain xmin xmax ymin ymax zmin zmax) !n !t !m !(u0, v0, w
 -- >       +--+-----------------+--+
 -- >          |        I2       |
 -- >          +-----------------+
+--
+-- PRNG state requires this to be a monadic action.
 openBoundaryInjection :: PrimMonad m =>
                          Gen (PrimState m)
                       -> Domain
@@ -148,18 +149,16 @@ openBoundaryInjection :: PrimMonad m =>
                       -> Vec3
                       -- ^ Flow velocity.
                       -> m Ensemble
-openBoundaryInjection !g !d@(Domain xmin xmax ymin ymax zmin zmax)
-                      !ex !n !t !m !flow =
+openBoundaryInjection !g !domain !ex !n !t !m !flow =
     let
-        (w, l, h) = getDimensions d
-        (cx, cy, cz) = getCenter d
+        (w, l, h) = getDimensions domain
+        (cx, cy, cz) = getCenter domain
         d1 = makeDomain (cx - (w + ex) / 2, cy, cz) ex l h
         d2 = makeDomain (cx + (w + ex) / 2, cy, cz) ex l h
         d3 = makeDomain (cx, cy + (l + ex) / 2, cz) w ex h
         d4 = makeDomain (cx, cy - (l + ex) / 2, cz) w ex h
         d5 = makeDomain (cx, cy, cz - (h + ex) / 2) w l ex
         d6 = makeDomain (cx, cy, cz + (h + ex) / 2) w l ex
-        (++) = (VU.++)
     in do
       v1 <- spawnParticles g d1 n t m flow
       v2 <- spawnParticles g d2 n t m flow
@@ -167,4 +166,32 @@ openBoundaryInjection !g !d@(Domain xmin xmax ymin ymax zmin zmax)
       v4 <- spawnParticles g d4 n t m flow
       v5 <- spawnParticles g d5 n t m flow
       v6 <- spawnParticles g d6 n t m flow
-      return $ v1 ++ v2 ++ v3 ++ v4 ++ v5 ++ v6
+      return $ VU.concat [v1, v2, v3, v4, v5, v6]
+
+
+-- | Filter out particles which are outside of the domain.
+--
+-- This is a monadic action because 'selectP' is used, which does
+-- 'unsafePerformIO' under the hood.
+clipToDomain :: Monad m => Domain -> Ensemble -> m Ensemble
+clipToDomain !(Domain xmin xmax ymin ymax zmin zmax) ens = 
+    let
+        arr :: ParEnsemble
+        arr = R.fromUnboxed (R.ix1 $ size) ens
+        size = VU.length ens
+        -- | Get i-th particle from ensemble
+        getter :: Int -> Particle
+        getter i = (R.!) arr (R.ix1 i)
+        {-# INLINE getter #-}
+        -- | Check if particle is in the domain.
+        pred :: Int -> Bool
+        pred i =
+            let 
+                ((x, y, z), _) = getter i
+            in
+              xmax >= x && x >= xmin &&
+              ymax >= y && y >= ymin &&
+              zmax >= z && z >= zmin
+        {-# INLINE pred #-}
+    in do
+      return $ R.toUnboxed $ R.selectP pred getter size arr
