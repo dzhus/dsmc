3610a6e2fe020c3e900dfd985efdc53cbb0b5d94
Dmitry Dzhus <dima@dzhus.org>
2012-06-05 23:12:16 +0400

    +DSMC.hit

--- src/DSMC.hs
+++ src/DSMC.hs
@@ -1,16 +1,22 @@
+{-# LANGUAGE BangPatterns #-}
+
 {-|
 
-  Simulation procedures.
+Simulation procedures.
 
 -}
 module DSMC
-    (advance)
+    ( advance
+    , hit)
 
 where
 
 import qualified Data.Array.Repa as R
 
+import Data.Strict.Maybe as S
+
 import DSMC.Particles
+import DSMC.Traceables
 import DSMC.Types
 import DSMC.Util.Vector
 
@@ -18,8 +24,8 @@ import DSMC.Util.Vector
 -- | Update particle position and velocity after specular hit given a
 -- normalized normal vector of surface in hit point and time since hit
 -- (not positive)
-reflectSpecular :: Particle -> Vec3 -> Time -> Particle
-reflectSpecular (pos, v) n t =
+reflectSpecular :: Particle -> Time -> Vec3 -> Particle
+reflectSpecular !(pos, v) !t !n =
     move (-1 * t) (pos, v <-> (n .^ (v .* n) .^ 2))
 
 
@@ -32,3 +38,24 @@ advance :: Monad m =>
         -> Ensemble
         -> m Ensemble
 advance dt ens = R.computeP $ R.map (move dt) ens
+
+
+-- | Reflect particles which have collided with body.
+hit :: Monad m =>
+       Time
+    -> Body
+    -> Ensemble
+    -> m Ensemble
+hit dt b ens =
+    let
+        hits = R.map (hitPoint dt b) ens
+        maybeReflect = (\(!p) (!h) ->
+                            case h of
+                              S.Just (HitPoint t (S.Just n)) ->
+                                  reflectSpecular p t n
+                              _ -> p)
+        {-# INLINE maybeReflect #-}
+        -- Reflect those particles which are not
+        reflected = R.zipWith maybeReflect ens hits
+    in
+      R.computeP $ reflected
