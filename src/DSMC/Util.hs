237333c96ae4e245a1a4a63d7cb6df5fbf05a34d
Dmitry Dzhus <dima@dzhus.org>
2012-06-05 13:35:59 +0400

    +purifyRandomST

--- src/DSMC/Util.hs
+++ src/DSMC/Util.hs
@@ -1,25 +1,32 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE Rank2Types #-}
 
 -- | Utility functions.
 
 module DSMC.Util
     ( solveq
     , SquareRoots
+    , purifyRandomST
     )
 
 where
 
 import Prelude hiding (Just, Nothing, Maybe, fst)
 
+import Control.Monad.ST
+
 import Data.Strict.Maybe
 import Data.Strict.Tuple
+
+import System.Random.MWC
+
 
 type SquareRoots = Maybe (Pair Double Double)
 
 -- | Solve quadratic equation @ax^2 + bx + c = 0@.
 --
 -- If less than two roots exist, Nothing is returned.
-solveq :: Double 
+solveq :: Double
        -- ^ a
        -> Double
        -- ^ b
@@ -38,3 +45,9 @@ solveq !a !b !c
       r1 =   r - s
       r2 =   r + s
 {-# INLINE solveq #-}
+
+
+-- | Conver ST action with PRNG state into a pure function of seed.
+purifyRandomST :: (forall s.GenST s -> ST s a) -> Seed -> a
+purifyRandomST f seed = runST $ restore seed >>= f
+{-# INLINE purifyRandomST #-}
