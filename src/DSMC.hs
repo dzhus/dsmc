dc657eef9f170b16d779feec5e121eef90bd7246
Dmitry Dzhus <dima@dzhus.org>
2012-06-06 00:10:06 +0400

    Combine DSMC.hit and advance into one operation for fusion. We clip after hit step anyways.

--- src/DSMC.hs
+++ src/DSMC.hs
@@ -7,7 +7,7 @@ Simulation procedures.
 -}
 module DSMC
     ( advance
-    , hit)
+    )
 
 where
 
@@ -29,33 +29,23 @@ reflectSpecular !(pos, v) !t !n =
     move (-1 * t) (pos, v <-> (n .^ (v .* n) .^ 2))
 
 
--- | Advance particles in space without collisions.
---
--- We use Repa parallel 'R.map', thus it's a monadic action.
+-- | Advance particles in space without intermolecular collisions, but
+-- reflect particles which have collided with body.
 advance :: Monad m =>
-           Double
-        -- ^ Time step.
+           Time
+        -> Body
         -> Ensemble
         -> m Ensemble
-advance dt ens = R.computeP $ R.map (move dt) ens
-
-
--- | Reflect particles which have collided with body.
-hit :: Monad m =>
-       Time
-    -> Body
-    -> Ensemble
-    -> m Ensemble
-hit dt b ens =
+advance !dt !b ens =
     let
-        hits = R.map (hitPoint dt b) ens
-        maybeReflect = (\(!p) (!h) ->
-                            case h of
+        moved = R.map (move dt) ens
+        maybeReflect = (\(!p) ->
+                            case (hitPoint dt b p) of
                               S.Just (HitPoint t (S.Just n)) ->
                                   reflectSpecular p t n
                               _ -> p)
         {-# INLINE maybeReflect #-}
         -- Reflect those particles which are not
-        reflected = R.zipWith maybeReflect ens hits
+        reflected = R.map maybeReflect moved
     in
       R.computeP $ reflected
