43797cf3d9303bc59f0b03cc31954d449de73877
Dmitry Dzhus <dima@dzhus.org>
2012-06-13 23:06:36 +0400

    Diffuse reflection model

--- src/DSMC/Surface.hs
+++ src/DSMC/Surface.hs
@@ -17,6 +17,7 @@ where
 import Control.Monad.ST
 
 import System.Random.MWC
+import System.Random.MWC.Distributions (normal)
 
 import DSMC.Util.Constants
 import DSMC.Util.Vector
@@ -28,12 +29,19 @@ type Reflector s = GenST s -> Vec3 -> Vec3 -> ST s Vec3
 
 
 data Surface = CLL { bodyTemperature :: !Double
+                   -- ^ Absolute temperature of surface.
                    , alpha :: !Double
                    -- ^ Kinetic energy accomodation for normal velocity component.
                    , sigma :: !Double
                    -- ^ Accomodation for tangential momentum.
                    } |
                -- ^ Cercignani-Lampis-Lord model.
+               Diffuse { bodyTemperature :: !Double
+                       -- ^ Absolute temperature of surface.
+                       , mass :: !Double
+                       -- ^ Mass of reflected molecules (equal to
+                       -- that in incident flow).
+                       } |
                Mirror
                -- ^ Surface with specular reflection.
 
@@ -66,4 +74,24 @@ makeReflector (CLL t alphanor sigmatan) =
         {-# INLINE cll #-}
     in
       cll
+
 makeReflector Mirror =  \_ !v !n -> return $! v <-> (n .^ (v .* n) .^ 2)
+
+makeReflector (Diffuse t m) =
+    let
+        s = sqrt $ boltzmann * t / m
+        beta = 1 / (s * (sqrt 2))
+        diffuse :: Reflector s
+        diffuse g n vel = 
+            let
+                e1 = normalize $ n >< vel
+                e2 = normalize $ n >< e1
+            in do
+              w <- normal 0 s g
+              v <- normal 0 s g
+              u' <- uniform g
+              let u = (sqrt (- (log u'))) / beta
+              return $ (e1 .^ v) <+> (e2 .^ w) <+> (n .^ u)
+        {-# INLINE diffuse #-}
+    in
+      diffuse
