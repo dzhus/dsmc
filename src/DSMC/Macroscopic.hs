36ea1cfbd3b783ccda7181181615a6b62339cf3b
Dmitry Dzhus <dima@dzhus.org>
2012-07-23 20:16:35 +0400

    Improvements in Macroscopic module. Implement `updateSamples`, fix `initializeSamples`.
    Comments improved, unused declaration dropped
    
    Reorder data in MacroscopicSample

--- src/DSMC/Macroscopic.hs
+++ src/DSMC/Macroscopic.hs
@@ -11,78 +11,69 @@ are then collected in each cell for a certain number of time steps.
 -}
 
 module DSMC.Macroscopic
-    ( MacroCell
-    , Velocity
+    ( MacroSamples
     , initializeSamples
     , updateSamples
     )
 
 where
 
-import Prelude hiding (Just, Nothing, Maybe, fst)
+import Prelude hiding (Just, Nothing, Maybe)
 
 import Control.Monad.ST
 
 import Data.Strict.Maybe
 
 import qualified Data.Array.Repa as R
-import qualified Data.Array.Repa.Repr.Vector as R
-import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
 
 import DSMC.Cells
 import DSMC.Particles
+import DSMC.Util
 import DSMC.Util.Vector
 
--- | Macroscopic parameters calculated in every cell: mean absolute
--- velocity, mean square of thermal velocity and particle count.
-type MacroParameters = (Vec3, Double, Int)
+-- | Macroscopic parameters calculated in every cell: particle count,
+-- mean absolute velocity, mean square of thermal velocity.
+type MacroParameters = (Int, Vec3, Double)
 
 
 -- | Vector which stores macroscropic parameters in each cell for
 -- every time step calculated during macroscopic sampling collection
 -- step.
 --
--- We store samples for time steps linearly for consecutive time
+-- We store samples for the whole domain linearly for consecutive time
 -- steps, so if there're @N@ cells and @M@ time steps, the size of
 -- array is @N*M@, where first N elements store samples from the first
 -- time step, next N elements store samples from the second etc.
 --
+-- Parameters sampled in @i@-th cell on @j@-th step are stored under
+-- index @j*N+i@.
+--
 -- If the vector is reshaped into two-dimensional array, then first
--- dimension is time step index, where 0 is the time step when
+-- dimension is the time step index, where 0 is the time step when
 -- averaging starts; second dimension is the cell index as returned by
 -- function produced with corresponding 'makeRegularIndexer'.
 type MacroSamples = VU.Vector MacroParameters
---type MacroSamples = R.Array R.U R.DIM2 MacroParameters
-
-
--- | Sampling cell is attached to certain point in space and has a
--- number of specimen.
-type MacroCell = (Point, CellContents)
-
-
--- | Flow velocity.
-type Velocity = Vec3
-
-
--- | A function which calculates instantaneous macroscopic average of
--- a parameter using molecules in a cell.
-type CellSampler a = CellContents -> Maybe a
 
 
 -- | Parameters in empty cell.
 emptySample :: MacroParameters
-emptySample = ((0, 0, 0), 0, 0)
+emptySample = (0, (0, 0, 0), 0)
 
 
 -- | Create empty 'MacroSamples' array.
 initializeSamples :: Int
+                  -- ^ Cell count.
+                  -> Int
                   -- ^ For how many time steps to collect samples.
                   -> MacroSamples
-initializeSamples n = VU.replicate n emptySample
+initializeSamples cellCount ts = VU.replicate (ts * cellCount) emptySample
 
 
 -- | Gather samples from ensemble.
+--
+-- Not that bounds are not checked.
 updateSamples :: Monad m =>
                  Int
               -- ^ Index of current time step, with 0 being the step
@@ -91,22 +82,35 @@ updateSamples :: Monad m =>
               -- ^ Cell count and classifier.
               -> Ensemble
               -> MacroSamples
-              -- ^ Current sample information to be updated.
+              -- ^ Current sample information to be updated, if
+              -- there's any.
               -> m MacroSamples
-updateSamples n sorting ens samples =
+updateSamples n sorting@(cellCount, _) ens oldSamples =
     let
         !sorted = sortParticles sorting ens
+        -- Starting index for current step in grand vector of samples
+        !stepStart = n * cellCount
+    in return $ runST $ do
         -- Sampling results from current step
-    in do
+      !stepSamples <- R.computeP $
+                      cellMap (\_ c -> sampleMacroscopic c) sorted
+
+      samples' <- VU.unsafeThaw oldSamples
+      iforM_ (R.toUnboxed $ stepSamples)
+                 (\(i, macroParams) -> do
+                    VUM.write samples' (stepStart + i) macroParams
+                    return ())
+      samples <- VU.unsafeFreeze samples'
+
       return samples
     
 
 -- | Sample macroscopic values in a cell.
-sampleMacroscopic :: CellContents -> MacroParameters
-sampleMacroscopic !ens =
-    case VU.null ens of
-      True -> emptySample
-      False ->
+sampleMacroscopic :: Maybe CellContents -> MacroParameters
+sampleMacroscopic !c =
+    case c of
+      Nothing -> emptySample
+      Just ens ->
           let
               -- Particle count
               n = VU.length ens
@@ -122,13 +126,4 @@ sampleMacroscopic !ens =
                                     (thrm .* thrm))
                       ens) * s
           in
-            (m1, c2, n)
-
-
-printVels :: V.Vector (Maybe (Point, Velocity)) -> IO ()
-printVels samples =
-    V.forM_ samples
-         (\mb -> do case mb of
-                      Nothing -> return ()
-                      Just ((x, y, z), (u, v, w)) ->
-                          putStrLn $ unwords (map show [x, y, z, u, v, w]))
+            (n, m1, c2)
