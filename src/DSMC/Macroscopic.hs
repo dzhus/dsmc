8b42e02ae6dde4cc2097e38f07456dfdd14fea7a
Dmitry Dzhus <dima@dzhus.org>
2012-07-25 00:05:28 +0400

    Keep all macroscopic samples in one array

--- src/DSMC/Macroscopic.hs
+++ src/DSMC/Macroscopic.hs
@@ -26,7 +26,6 @@ where
 
 import Prelude hiding (Just, Nothing, Maybe)
 
-import Control.Monad.ST
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State.Strict
@@ -35,48 +34,36 @@ import Data.Strict.Maybe
 
 import qualified Data.Array.Repa as R
 import qualified Data.Vector.Unboxed as VU
-import qualified Data.Vector.Unboxed.Mutable as VUM
 
 import DSMC.Cells
 import DSMC.Particles
-import DSMC.Util
 import DSMC.Util.Vector
 
 -- | Macroscopic parameters calculated in every cell: particle count,
 -- mean absolute velocity, mean square of thermal velocity.
 --
 -- Note the lack of root on thermal velocity!
-type MacroParameters = (Int, Vec3, Double)
+type MacroParameters = (Double, Vec3, Double)
 
 
--- | Vector which stores macroscropic parameters in each cell for
--- every time step calculated during macroscopic sampling collection
--- step.
+-- | Vector which stores averaged macroscropic parameters in each
+-- cell.
 --
--- We store samples for the whole domain linearly for consecutive time
--- steps, so if there're @N@ cells and @M@ time steps for averaging,
--- the size of array is @N*M@, where first N elements store samples
--- from the first time step, next N elements store samples from the
--- second etc.
---
--- Parameters sampled in @i@-th cell on @j@-th step are stored under
--- index @j*N+i@.
---
--- If the vector is reshaped into two-dimensional array, then first
--- dimension is the time step index, where 0 is the time step when
--- averaging starts; second dimension is the cell index as returned by
--- function produced with corresponding 'makeRegularIndexer'.
-type MacroSamples = VU.Vector MacroParameters
+-- If samples are collected for M iterations, then this vector is
+-- built as a sum of vectors @V1, .. VM@, where @Vi@ is vector of
+-- parameters sampled on @i@-th time step divided by @M@.
+type MacroSamples = R.Array R.U R.DIM1 MacroParameters
 
 
 -- | Monad which keeps track of sampling process data and stores
 -- options of macroscopic sampling.
 --
--- State contains averaging steps left and current samples. If
+-- State contains counter for averaging steps left and current samples
+-- (note that this is a partial sum till sampling is complete). If
 -- Nothing, then averaging has not started yet.
 --
 -- We use this to make sure that only safe values for cell count and
--- time steps are used in 'updateSamples' and 'averageSamples' (that
+-- classifier are used in 'updateSamples' and 'averageSamples' (that
 -- may otherwise cause unbounded access errors).
 type MacroSamplingMonad =
     StateT (Maybe (Int, MacroSamples)) (Reader MacroSamplingOptions)
@@ -84,7 +71,7 @@ type MacroSamplingMonad =
 
 -- | Options for macroscopic sampling: uniform grid and averaging
 -- steps count.
-data MacroSamplingOptions = 
+data MacroSamplingOptions =
     MacroSamplingOptions { _sorting :: (Int, Classifier)
                          , _indexer :: Int -> Point
                          , _averagingSteps :: Int
@@ -97,8 +84,8 @@ emptySample = (0, (0, 0, 0), 0)
 
 
 -- | Run 'MacroSamplingMonad' action with given sampling options.
-startMacroSampling :: MacroSamplingMonad r -> 
-                      MacroSamplingOptions -> 
+startMacroSampling :: MacroSamplingMonad r ->
+                      MacroSamplingOptions ->
                      (r, Maybe (Int, MacroSamples))
 startMacroSampling f opts = runReader (runStateT f Nothing) opts
 
@@ -106,61 +93,70 @@ startMacroSampling f opts = runReader (runStateT f Nothing) opts
 -- | Create empty 'MacroSamples' array.
 initializeSamples :: Int
                   -- ^ Cell count.
-                  -> Int
-                  -- ^ Averaging steps.
                   -> MacroSamples
-initializeSamples cellCount ts = VU.replicate (ts * cellCount) emptySample
+initializeSamples cellCount = fromUnboxed1 $
+                              VU.replicate cellCount emptySample
 
 
 -- | Gather samples from ensemble. Return True if sampling is
 -- finished, False otherwise.
 updateSamples :: Ensemble
               -> MacroSamplingMonad Bool
-updateSamples ens = do
-  samples <- get
+updateSamples ens =
+    let
+        addCellParameters :: MacroParameters
+                          -> MacroParameters
+                          -> MacroParameters
+        addCellParameters !(n1, v1, c1) !(n2, v2, c2) =
+            (n1 + n2, v1 <+> v2, c1 + c2)
+    in do
+      sorting@(cellCount, _) <- lift $ asks _sorting
 
-  sorting@(cellCount, _) <- lift $ asks _sorting
+      maxSteps <- lift $ asks _averagingSteps
 
-  maxSteps <- lift $ asks _averagingSteps
+      sampling <- get
 
-  let (n, oldSamples) =
-          case samples of
-            Nothing -> (maxSteps, initializeSamples cellCount maxSteps)
-            -- Just (0, _) -> error
-            Just o -> o
-      -- Sort particles into macroscopic cells for sampling
-      sorted = sortParticles sorting ens
-      -- Starting index for current step in grand vector of samples
-      stepStart = (maxSteps - n) * cellCount
-      -- Update samples vector
-      newSamples = runST $ do
-        -- Sampling results from current step
-        stepSamples <- R.computeP $
-                        cellMap (\_ c -> sampleMacroscopic c) sorted
+      -- n is steps left for averaging
+      let (n, oldSamples) =
+              case sampling of
+                Nothing -> (maxSteps, initializeSamples cellCount)
+                Just o -> o
+          weight = 1 / fromIntegral maxSteps
+          -- Sort particles into macroscopic cells for sampling
+          sorted = sortParticles sorting ens
+          -- Sampling results from current step
+          stepSamples = cellMap (\_ c -> sampleMacroscopic c weight) sorted
+       -- Add samples from current step to all sum of samples collected so
+       -- far
+      !newSamples <- R.computeP $ 
+                     R.zipWith addCellParameters oldSamples stepSamples
 
-        samples' <- VU.unsafeThaw oldSamples
-        iforM_ (R.toUnboxed $ stepSamples)
-                   (\(i, macroParams) -> do
-                      VUM.unsafeWrite samples' (stepStart + i) macroParams
-                      return ())
-        VU.unsafeFreeze samples'
-  -- Update state of sampling process
-  put $ Just (n - 1, newSamples)
+      -- Update state of sampling process
+      put $ Just (n - 1, newSamples)
 
-  return $ (n - 1) == 0
+      return $ (n - 1) == 0
 
 -- | Sample macroscopic values in a cell.
-sampleMacroscopic :: Maybe CellContents -> MacroParameters
-sampleMacroscopic !c =
+sampleMacroscopic :: Maybe CellContents
+                  -> Double
+                  -- ^ Multiply all sampled parameters by this number,
+                  -- which is the statistical weight of one sample.
+                  -- Typically this is inverse to the amount of steps
+                  -- used for averaging.
+                  -> MacroParameters
+sampleMacroscopic !c !weight =
     case c of
       Nothing -> emptySample
       Just ens ->
           let
               -- Particle count
-              n = VU.length ens
-              s = 1 / fromIntegral n
+              n = fromIntegral $ VU.length ens
+              -- Particle averaging factor
+              s = 1 / n
+              -- Velocities factor
+              sv = s * weight
               -- Mean absolute velocity
-              m1 = (VU.foldl' (\v0 (_, v) -> v0 <+> v) (0, 0, 0) ens) .^ s
+              m1 = (VU.foldl' (\v0 (_, v) -> v0 <+> v) (0, 0, 0) ens) .^ sv
               -- Mean square thermal velocity
               c2 = (VU.foldl' (+) 0 $
                       VU.map (\(_, v) ->
@@ -168,6 +164,6 @@ sampleMacroscopic !c =
                                     thrm = (v <-> m1)
                                   in
                                     (thrm .* thrm))
-                      ens) * s
+                      ens) * sv
           in
-            (n, m1, c2)
+            (n * weight, m1, c2)
