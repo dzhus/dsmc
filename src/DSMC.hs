58beffefd5a5908c9f7202c9d24bbbd485492198
Dmitry Dzhus <dima@dzhus.org>
2012-07-26 02:48:06 +0400

    Split `motion` and `reflect` into separate module.

--- src/DSMC.hs
+++ src/DSMC.hs
@@ -18,73 +18,20 @@ import Control.Monad.Primitive (PrimMonad)
 
 import Control.Parallel.Stochastic
 
-import Data.Functor
-
 import qualified Data.Array.Repa as R
-
-import qualified Data.Strict.Maybe as S
-
-import qualified Data.Vector.Unboxed as VU
 
 import System.Random.MWC
 
 import DSMC.Cells
 import DSMC.Domain
 import DSMC.Macroscopic
+import DSMC.Motion
 import DSMC.Particles
 import DSMC.Surface
 import DSMC.Traceables hiding (trace)
 import DSMC.Util
 
-import Control.Monad.ST
-
 import Debug.Trace
-
-
--- | Sequential action to move particles and consider particle-body
--- collisions.
-reflect :: GenST s
-        -> Body
-        -> Time
-        -> Reflector s
-        -> VU.Vector Particle
-        -> ST s (VU.Vector Particle)
-reflect g body dt reflector ens = do
-  VU.forM ens $ \pcl -> do
-    -- Particle after collisionless motion
-    let movedPcl = move dt pcl
-    case (hitPoint dt body movedPcl) of
-      -- Enjoy your convex-only case.
-      S.Just (HitPoint th (S.Just n)) ->
-          let
-              -- Position and velocity at hit point
-              (pos', v) = move th pcl
-          in do
-            -- Sample velocity for reflected particle
-            vR <- reflector g n v
-            -- Move particle away from surface with new velocity
-            return $ move (-th) (pos', vR)
-      _ -> return $ movedPcl
-
-
--- | Collisionless motion step.
-motion :: ParallelSeeds
-       -> Body
-       -> Time
-       -> Surface
-       -> Ensemble
-       -> (Ensemble, ParallelSeeds)
-motion gs b dt surf ens =
-    let
-        vs :: [VU.Vector Particle]
-        -- | Since 'reflect' is sequential, we split ensemble into N
-        -- slices and process them in parallel.
-        !vs = splitIn (R.toUnboxed ens) (length gs)
-        reflector = makeReflector surf
-        v' :: [(VU.Vector Particle, Seed)]
-        !v' = parMapST (\g e -> reflect g b dt reflector e) $ zip vs gs
-    in
-      (fromUnboxed1 $ VU.concat $ fst <$> v', snd <$> v')
 
 
 -- | Perform DSMC simulation, return final particle distribution and
@@ -169,7 +116,10 @@ simulate domain body flow dt emptyStart ex sepsilon ssteps (mx, my, mz) gsplit =
                 field <- getField
                 return (ens', field)
     in do
+      -- Global seeds
       gs <- replicateM gsplit $ create >>= save
+
+      -- Interface domain seeds
       s1 <- create >>= save
       s2 <- create >>= save
       s3 <- create >>= save
---
+++ src/DSMC/Motion.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE BangPatterns #-}
+
+{-|
+
+Collisionless motion.
+
+-}
+
+module DSMC.Motion
+    ( motion
+    )
+
+where
+
+import Control.Parallel.Stochastic
+
+import Data.Functor
+
+import qualified Data.Strict.Maybe as S
+
+import qualified Data.Array.Repa as R
+
+import qualified Data.Vector.Unboxed as VU
+
+import System.Random.MWC
+
+import DSMC.Particles
+import DSMC.Surface
+import DSMC.Traceables hiding (trace)
+import DSMC.Util
+
+import Control.Monad.ST
+
+-- | Sequential action to move particles and consider particle-body
+-- collisions.
+reflect :: GenST s
+        -> Body
+        -> Time
+        -> Reflector s
+        -> VU.Vector Particle
+        -> ST s (VU.Vector Particle)
+reflect g body dt reflector ens = do
+  VU.forM ens $ \pcl -> do
+    -- Particle after collisionless motion
+    let movedPcl = move dt pcl
+    case (hitPoint dt body movedPcl) of
+      -- Enjoy your convex-only case.
+      S.Just (HitPoint th (S.Just n)) ->
+          let
+              -- Position and velocity at hit point
+              (pos', v) = move th pcl
+          in do
+            -- Sample velocity for reflected particle
+            vR <- reflector g n v
+            -- Move particle away from surface with new velocity
+            return $ move (-th) (pos', vR)
+      _ -> return $ movedPcl
+
+
+-- | Collisionless motion step.
+motion :: ParallelSeeds
+       -> Body
+       -> Time
+       -> Surface
+       -> Ensemble
+       -> (Ensemble, ParallelSeeds)
+motion gs b dt surf ens =
+    let
+        vs :: [VU.Vector Particle]
+        -- | Since 'reflect' is sequential, we split ensemble into N
+        -- slices and process them in parallel.
+        !vs = splitIn (R.toUnboxed ens) (length gs)
+        reflector = makeReflector surf
+        v' :: [(VU.Vector Particle, Seed)]
+        !v' = parMapST (\g e -> reflect g b dt reflector e) $ zip vs gs
+    in
+      (fromUnboxed1 $ VU.concat $ fst <$> v', snd <$> v')
+
